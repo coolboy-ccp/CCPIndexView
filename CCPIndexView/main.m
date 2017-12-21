3b66493d2e1dd360568c94c85685a2f3dadf75bf
cool-ccp <chuchengpeng@chuchengpengs-MacBook-Pro.local>
2017-12-21 13:32:50 +0800

    Initial Commit

---
+++ CCPIndexView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CCPIndexView
+//
+//  Created by chuchengpeng on 2017/12/21.
+//  Copyright © 2017年 chuchengpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
